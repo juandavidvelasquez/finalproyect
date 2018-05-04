2e6eb9a0f59097d4f0e0ca65ff0947dd1de8c103
Unknown <videojuegosjuanda666@gmail.com>
2018-05-04 12:13:29 -0500

    proyecto final

---
+++ Assets/Scripts/Coches.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// sirve para simular la inteligencia artifial del movimiento de los coches  
+public class Coches : MonoBehaviour {
+	public float speed;
+	public Transform target;
+
+	void Start () {
+			transform.LookAt(new Vector3(target.position.x,target.position.y,target.position.z));
+	}
+	
+	
+	void Update () 
+	{
+		transform.Translate(new Vector3(0,0,Time.deltaTime));
+
+	}
+
+void OnTriggerEnter(Collider other)
+{
+	if(other.tag == "Waypoint")
+	{
+		target = other.gameObject.GetComponent<Waypoint>().nuevoPunto;
+		transform.LookAt(new Vector3(target.position.x,transform.position.y,target.position.z));
+
+	}
+
+}
+
+}
+
---
+++ Assets/Scripts/RandomObjects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// el script sirve para instanciar item en una posicion aleatoria
+public class RandomObjects : MonoBehaviour {
+
+	public Vector3[] posicion;
+
+
+	void Start ()
+	 {
+		 int RandomNum = Random.Range(0,posicion.Length);
+		 transform.position = posicion[RandomNum];
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
