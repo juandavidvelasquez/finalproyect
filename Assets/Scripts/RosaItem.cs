06e538b0e97793277584023f2a8cf6ba72935163
Unknown <videojuegosjuanda666@gmail.com>
2018-05-09 17:04:38 -0500

    final

--- Assets/Scripts/RosaItem.cs
+++ Assets/Scripts/RosaItem.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rosa : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class RosaItem : MonoBehaviour {
+	// este script sirve para simular que retrocede en el tiempo tomando las posiciones por las que transito el jugador 
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  private ArrayList Movements = new ArrayList();
+     private int MovementIndex = 0;
+     private bool rewinding;
+ 
+     void Update ()
+     {
+         if (!rewinding){
+             Movements.Add(transform.position);
+             MovementIndex++;
+         }
+ 
+         if(MovementIndex > Movements.Count - 1) {
+             MovementIndex = Movements.Count;
+         }
+ 
+         if(Input.GetKey("e")) {
+             rewinding = true;
+             RewindTime();
+         } else {
+             rewinding = false;
+         }
+     }
+ 
+     void RewindTime ()
+     {
+         MovementIndex--;
+         if (MovementIndex > 0) {
+             transform.position = (Vector3) Movements[MovementIndex];
+             Movements.RemoveAt(MovementIndex);
+         }
+     }
 }
+
+	
+	
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trigger : MonoBehaviour {
+// este script sirve para activar la clase que permite retroceder el tiempo al tomar el item de la rosa 
+	public Text texto;
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.tag == ("Rose"))
+		{
+			Instantiate (texto,transform.position,transform.rotation);
+			GetComponent<RosaItem>();
+		}else{
+			if(Input.GetKey(KeyCode.E))
+			{
+				Destroy(texto);
+				Destroy(gameObject);
+			}
+		}
+
+	}
+}
